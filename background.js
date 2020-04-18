215d27f07cfb65945bda65eec23632b9c9972fcb
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2020-04-18 19:44:12 +0200

    1.1 od gauge fix

---
+++ background.js
@@ -0,0 +1,29 @@
+var Background = (function(self){
+
+    let whitelist = ['attack', 'button', '/c/', 'summon', 'abilit', 'status', 'mode_gauge_yel'/*, 'btn'*/];
+
+    function urlcheck(url){
+        var index = url.indexOf("?s=");
+        if(index == -1) index = url.length;
+        if(url.indexOf(".png") == index - 4 || url.indexOf(".jpg") == index - 4)
+        {
+            for(var i = 0; i < whitelist.length; ++i)
+                if(url.indexOf(whitelist[i]) != -1) return false;
+            return true;
+        }
+        return false;
+    }
+
+    chrome.webRequest.onBeforeRequest.addListener(
+        function(detail){
+            if(urlcheck(detail.url)) return {cancel: true};
+        },
+        {urls: ["*://game-a.granbluefantasy.jp/*",
+           "*://game-a1.granbluefantasy.jp/*",
+           "*://game-a2.granbluefantasy.jp/*",
+           "*://game-a3.granbluefantasy.jp/*",
+           "*://game-a4.granbluefantasy.jp/*",
+           "*://game-a5.granbluefantasy.jp/*"]},
+        ["blocking"]
+    );
+})(window);
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Granblue Asset Blocker  
+To keep your potato as cool as possible  
+  
+# Usage  
+Extension menu > enable Dev mode > load unpacked > select the folder containing those files
